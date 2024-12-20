e2605ed79efe374715143f045257a91052693b31
Riccardoalfieri2003 <162313163+Riccardoalfieri2003@users.noreply.github.com>
2024-12-20 16:16:56 +0100

    Gestione eccezioni e numero di immagini

--- Codice/instagramScraper/post_informations.py
+++ Codice/instagramScraper/post_informations.py
@@ -6,17 +6,25 @@ driver=createDriver()
 
 links=[
     'https://www.instagram.com/p/DCMFXrou83J/',
-    'https://www.instagram.com/p/DDLB-pfs2AS/'
+    'https://www.instagram.com/p/DDLB-pfs2AS/',
+    'https://www.instagram.com/p/DDpU1jDMDfX/',
+    'https://www.instagram.com/p/DCgfA9JS6Ud/'
 ]
 
 
 
 for link in links:
-    name,luogo,like,descrizione,datetime,followerNumber=getInfos(driver,link)
+    try: name,luogo,like,descrizione,datetime,followerNumber,numImmagini=getInfos(driver,link)
+    except: 
+        driver.quit()
+        driver=createDriver()
+        name,luogo,like,descrizione,datetime,followerNumber,numImmagini=getInfos(driver,link)
+        
     print("Nome: ",name)
     print("Luogo: ", luogo)
     print("Like: ",like)
     print("Descrizione: ",descrizione)
     print("Datetime: ",datetime)
     print("Followers: ",followerNumber)
+    print("numImmagini: ",numImmagini)
     print("\n")
