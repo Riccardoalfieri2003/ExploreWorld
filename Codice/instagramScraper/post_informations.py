3337dd6c58fb4299a9691fea71512ec5ad75f8ff
Riccardoalfieri2003 <162313163+Riccardoalfieri2003@users.noreply.github.com>
2024-12-20 14:04:31 +0100

    Aggiunta print dei followers

--- Codice/instagramScraper/post_informations.py
+++ Codice/instagramScraper/post_informations.py
@@ -5,11 +5,18 @@ from create_driver import createDriver
 driver=createDriver()
 
 links=[
-    'https://www.instagram.com/p/DCMFXrou83J/'
+    'https://www.instagram.com/p/DCMFXrou83J/',
+    'https://www.instagram.com/p/DDLB-pfs2AS/'
 ]
 
 
 
 for link in links:
-    name,luogo,like,descrizione,datetime=getInfos(driver,link)
-    print(name,luogo,like,descrizione,datetime)
+    name,luogo,like,descrizione,datetime,followerNumber=getInfos(driver,link)
+    print("Nome: ",name)
+    print("Luogo: ", luogo)
+    print("Like: ",like)
+    print("Descrizione: ",descrizione)
+    print("Datetime: ",datetime)
+    print("Followers: ",followerNumber)
+    print("\n")
