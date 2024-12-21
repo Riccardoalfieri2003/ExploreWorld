5f4f6c369dbd671e6d78fd6fc7f48deeaaf02e94
FrancescoMascolo19 <162311967+FrancescoMascolo19@users.noreply.github.com>
2024-12-21 02:46:11 +0100

    Creazione e gestione del Dataset

--- Codice/instagramScraper/post_informations.py
+++ Codice/instagramScraper/post_informations.py
@@ -1,30 +1,54 @@
+import pandas as pd
 from instagram_scraper import getInfos
 from create_driver import createDriver
 
+# Creazione del driver
+driver = createDriver()
 
-driver=createDriver()
-
-links=[
-    'https://www.instagram.com/p/DCMFXrou83J/',
-    'https://www.instagram.com/p/DDLB-pfs2AS/',
-    'https://www.instagram.com/p/DDpU1jDMDfX/',
+# Lista dei link dei post
+links = [
+    #'https://www.instagram.com/p/DCMFXrou83J/',
+    #'https://www.instagram.com/p/DDLB-pfs2AS/',
+    #'https://www.instagram.com/p/DDpU1jDMDfX/',
     'https://www.instagram.com/p/DCgfA9JS6Ud/'
 ]
 
-
+# Lista per memorizzare i dati dei post
+data = []
 
 for link in links:
-    try: name,luogo,like,descrizione,datetime,followerNumber,numImmagini=getInfos(driver,link)
-    except: 
+    try:
+        # Estrazione delle informazioni
+        name, luogo, like, descrizione, datetime, followerNumber, numImmagini = getInfos(driver, link)
+        #print(f"Post: {link}, NumImmagini: {numImmagini}")
+    except:
+        # Ricrea il driver in caso di errore
         driver.quit()
-        driver=createDriver()
-        name,luogo,like,descrizione,datetime,followerNumber,numImmagini=getInfos(driver,link)
-        
-    print("Nome: ",name)
-    print("Luogo: ", luogo)
-    print("Like: ",like)
-    print("Descrizione: ",descrizione)
-    print("Datetime: ",datetime)
-    print("Followers: ",followerNumber)
-    print("numImmagini: ",numImmagini)
-    print("\n")
+        driver = createDriver()
+        name, luogo, like, descrizione, datetime, followerNumber, numImmagini = getInfos(driver, link)
+
+
+    # Salvataggio delle informazioni in un dizionario
+    data.append({
+        'Nome': name,
+        'Luogo': luogo,
+        'Like': like,
+        'Descrizione': descrizione,
+        'Datetime': datetime,
+        'Followers': followerNumber,
+        'NumImmagini': numImmagini,
+
+    })
+
+# Chiusura del driver
+driver.quit()
+
+print(data)
+# Creazione del DataFrame
+df = pd.DataFrame(data)
+
+# Mostra il DataFrame
+print(df)
+
+# Salvataggio del DataFrame in un file CSV
+df.to_csv('instagram_posts.csv', index=False)
